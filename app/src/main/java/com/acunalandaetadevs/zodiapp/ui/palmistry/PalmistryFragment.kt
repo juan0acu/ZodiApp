466e4023e685bd95d2a8048f60d9ab3131979cd1
Juan Acuna <juan0acu@gmail.com>
2023-11-15 23:24:27 -0300

    Primeras configuraciones antes de la navegaciones entre fragmentos

---
+++ app/src/main/java/com/acunalandaetadevs/zodiapp/ui/palmistry/PalmistryFragment.kt
@@ -0,0 +1,22 @@
+package com.acunalandaetadevs.zodiapp.ui.palmistry
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.acunalandaetadevs.zodiapp.databinding.FragmentPalmistryBinding
+
+class PalmistryFragment : Fragment() {
+    private var _binding: FragmentPalmistryBinding? = null
+    private val binding get() = _binding!!
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
+        return binding.root
+    }
+
+}
