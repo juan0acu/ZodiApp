d6134f748ea26a7c8726547829a63c66206314f9
Juan Acuna <juan0acu@gmail.com>
2023-11-17 22:47:43 -0300

    Configuración de Daggerhilt

---
+++ app/src/main/java/com/acunalandaetadevs/zodiapp/ZodiApp.kt
@@ -0,0 +1,7 @@
+package com.acunalandaetadevs.zodiapp
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class ZodiApp:Application()
--- app/src/main/java/com/acunalandaetadevs/zodiapp/ui/horoscope/HoroscopeFragment.kt
+++ app/src/main/java/com/acunalandaetadevs/zodiapp/ui/horoscope/HoroscopeFragment.kt
@@ -6,7 +6,9 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.acunalandaetadevs.zodiapp.databinding.FragmentHoroscopeBinding
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class HoroscopeFragment : Fragment() {
     private var _binding: FragmentHoroscopeBinding? = null
     private val binding get() = _binding!!
