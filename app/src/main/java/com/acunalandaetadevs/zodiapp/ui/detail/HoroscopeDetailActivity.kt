e83ee1d4959e42b6381a166b453c088717dd531f
Juan Acuna <juan0acu@gmail.com>
2023-11-22 22:52:03 -0300

    Agregando estados y conectado los estados con el viewModel y la pantalla de usuario

--- app/src/main/java/com/acunalandaetadevs/zodiapp/ui/detail/HoroscopeDetailActivity.kt
+++ app/src/main/java/com/acunalandaetadevs/zodiapp/ui/detail/HoroscopeDetailActivity.kt
@@ -3,22 +3,54 @@ package com.acunalandaetadevs.zodiapp.ui.detail
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.lifecycleScope
+import androidx.lifecycle.repeatOnLifecycle
 import androidx.navigation.navArgs
 import com.acunalandaetadevs.zodiapp.R
 import com.acunalandaetadevs.zodiapp.databinding.ActivityHoroscopeDetailBinding
 import dagger.hilt.android.AndroidEntryPoint
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
 
 @AndroidEntryPoint
 class HoroscopeDetailActivity : AppCompatActivity() {
     private lateinit var binding: ActivityHoroscopeDetailBinding
     private val horoscopeDetailViewModel by viewModels<HoroscopeDetailViewModel>()
 
-    private val args:HoroscopeDetailActivityArgs by navArgs()
+    private val args: HoroscopeDetailActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityHoroscopeDetailBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        args.type
+        initUI()
+    }
+
+    private fun initUI() {
+        initUIState()
+    }
+
+    private fun initUIState() {
+        lifecycleScope.launch {
+            repeatOnLifecycle(Lifecycle.State.STARTED) {
+                horoscopeDetailViewModel.state.collect {
+                    when (it) {
+                        HoroscopeDetailState.Loading -> loadingState()
+                        is HoroscopeDetailState.Error -> errorState()
+                        is HoroscopeDetailState.Success -> successState()
+                    }
+                }
+            }
+        }
+    }
+
+    private fun successState() {
+    }
+
+    private fun errorState() {
+    }
+
+    private fun loadingState() {
     }
 }
--- app/src/main/java/com/acunalandaetadevs/zodiapp/ui/detail/HoroscopeDetailViewModel.kt
+++ app/src/main/java/com/acunalandaetadevs/zodiapp/ui/detail/HoroscopeDetailViewModel.kt
@@ -2,6 +2,11 @@ package com.acunalandaetadevs.zodiapp.ui.detail
 
 import androidx.lifecycle.ViewModel
 import javax.inject.Inject
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
 
-class HoroscopeDetailViewModel @Inject constructor(): ViewModel() {
+class HoroscopeDetailViewModel @Inject constructor() : ViewModel() {
+
+    private var _state = MutableStateFlow<HoroscopeDetailState>(HoroscopeDetailState.Loading)
+    val state: StateFlow<HoroscopeDetailState> = _state
 }
