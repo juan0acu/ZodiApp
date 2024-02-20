e4da1f2f445d6a0c77dbbee6544a84c3e597b2b2
Juan Acuna <juan0acu@gmail.com>
2024-02-19 23:12:52 -0300

    Creación de Palmistry

--- app/src/main/java/com/acunalandaetadevs/zodiapp/ui/palmistry/PalmistryFragment.kt
+++ app/src/main/java/com/acunalandaetadevs/zodiapp/ui/palmistry/PalmistryFragment.kt
@@ -1,14 +1,18 @@
 package com.acunalandaetadevs.zodiapp.ui.palmistry
 
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.content.ContextCompat
 import androidx.core.content.PermissionChecker
 import androidx.fragment.app.Fragment
-import com.acunalandaetadevs.zodiapp.Manifest
 import com.acunalandaetadevs.zodiapp.databinding.FragmentPalmistryBinding
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -22,7 +26,7 @@ class PalmistryFragment : Fragment() {
         ActivityResultContracts.RequestPermission()
     ) { isGranted ->
         if (isGranted) {
-
+            startCamera()
         } else {
             Toast.makeText(requireContext(), "Debes aceptar los permisos", Toast.LENGTH_LONG).show()
         }
@@ -33,7 +37,7 @@ class PalmistryFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         if (checkCameraPermission()) {
-
+            startCamera()
         } else {
             requestPermissionLauncher.launch(CAMERA_PERMISSION)
         }
@@ -52,6 +56,30 @@ class PalmistryFragment : Fragment() {
     ): View {
         _binding = FragmentPalmistryBinding.inflate(layoutInflater, container, false)
         return binding.root
+    }
+
+    private fun startCamera(){
+        val camaraProviderFuture = ProcessCameraProvider.getInstance(requireContext())
+
+        camaraProviderFuture.addListener({
+            val cameraProvider: ProcessCameraProvider = camaraProviderFuture.get()
+
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                cameraProvider.unbindAll()
+
+                cameraProvider.bindToLifecycle(this, cameraSelector, preview)
+            }catch (e:Exception){
+                Log.e("Error", "Se pitio algo ${e.message}")
+            }
+        }, ContextCompat.getMainExecutor(requireContext()))
     }
 
     companion object{
